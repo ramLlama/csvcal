5e1de72fe53e1be423397cdad4dabf5018a89ede
Ram Raghunathan <ram.r@cs.cmu.edu>
2017-09-21 18:52:43 -0400

    Initial Commit: Add csv2ical.pl

---
+++ csv2ical.pl
@@ -0,0 +1,128 @@
+#! /usr/bin/env perl
+
+use 5.010;
+
+use warnings;
+use strict;
+
+use Data::Dumper;
+
+use Data::ICal;
+use Data::ICal::Entry::Event;
+use Date::ICal;
+use Date::Manip;
+use LWP::Simple;
+use Text::CSV;
+
+#############
+# Constants #
+#############
+my $SHEETSAPI_HOST = "https://sheets.googleapis.com";
+my $EMPTY_RES_SLOT = '';
+
+###############
+# Subroutines #
+###############
+sub process_reservation {
+  my ($calendar,
+      $owner,
+      $date,
+      $starttime,
+      $endtime,
+      $offset) = @_;
+  my $startdate = new Date::Manip::Date;
+  my $enddate = new Date::Manip::Date;
+
+  $startdate->parse("$date $starttime") and
+    die ("Error parsing \"$date $starttime\": " . ($startdate->err()));
+
+  $enddate->parse("$date $endtime") and
+    die ("Error parsing \"$date $endtime\": " . ($enddate->err()));
+
+  my $event = Data::ICal::Entry::Event->new();
+  $event->add_properties
+    (
+     summary => "Reservation for $owner"
+     , dtstart => Date::ICal->new(ical => $startdate->printf('%Y%m%dT%H%M%S')
+				  , offset => $offset)->ical
+     , dtend => Date::ICal->new(ical => $enddate->printf('%Y%m%dT%H%M%S')
+				, offset => $offset)->ical
+    );
+  $calendar->add_entry($event);
+}
+
+########
+# Main #
+########
+
+my $uri = $ARGV[0];
+my $offset = $ARGV[1];
+
+my $csv_raw = get $uri or die "Could not fetch CSV file from '$uri'";
+#say $csv_raw;
+
+my $csv = Text::CSV->new();
+
+my @rows = split("\n", $csv_raw);
+my %col2time = ();
+my $calendar = Data::ICal->new();
+for my $i (0 .. $#rows) {
+  my $row = $rows[$i] . "\n";
+
+  $csv->parse($row) or die "Could not parse row '$row'";
+  my @fields = $csv->fields();
+  if (0 == $i) {
+    # Header row
+    if ("Date" ne $fields[0]) {
+      die "Malformed CSV file";
+    }
+
+    # Populate col2time
+    for my $j (1 .. $#fields) {
+      $col2time{$j} = $fields[$j];
+    }
+  } else {
+    # This is a row of reservations
+    my $res_owner = undef;
+    my $res_starttime = undef;
+
+    my $date = $fields[0];
+    for my $j (1 .. $#fields) {
+      my $slot_owner = $fields[$j];
+
+      if (defined $res_owner) {
+	# I have an active reservation
+	if ($res_owner eq $slot_owner) {
+	  # The active reservation continues
+	  next;
+	} else {
+	  # I have a completed reservation to process
+	  process_reservation($calendar,
+			      $res_owner,
+			      $date,
+			      $res_starttime,
+			      $col2time{$j},
+			      $offset);
+
+	  my $res_owner = undef;
+	  my $res_starttime = undef;
+	  my $res_endtime = undef;
+	}
+      }
+
+      # At this point, I've processed any finished reservation if it existed.
+
+      if ($EMPTY_RES_SLOT ne $slot_owner) {
+	# Start a new reservation
+	if (defined $res_owner) {
+	  die "starting a new reservation with existing active one?!";
+	}
+
+	$res_owner = $slot_owner;
+	$res_starttime = $col2time{$j};
+      }
+    }
+  }
+}
+
+print ($calendar->as_string());
