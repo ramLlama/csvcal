0655699b252bbbf6d6d6923ca0ba9ed904dd47ee
Ram Raghunathan <ram.r@cs.cmu.edu>
2017-10-14 18:38:42 -0400

    csv2ical.pl: Support multi-day reservations.

--- csv2ical.pl
+++ csv2ical.pl
@@ -24,26 +24,27 @@ my $EMPTY_RES_SLOT = '';
 sub process_reservation {
   my ($calendar,
       $owner,
-      $date,
+      $startdate,
       $starttime,
+      $enddate,
       $endtime,
       $offset) = @_;
-  my $startdate = new Date::Manip::Date;
-  my $enddate = new Date::Manip::Date;
+  my $start = new Date::Manip::Date;
+  my $end = new Date::Manip::Date;
 
-  $startdate->parse("$date $starttime") and
-    die ("Error parsing \"$date $starttime\": " . ($startdate->err()));
+  $start->parse("$startdate $starttime") and
+    die ("Error parsing \"$startdate $starttime\": " . ($start->err()));
 
-  $enddate->parse("$date $endtime") and
-    die ("Error parsing \"$date $endtime\": " . ($enddate->err()));
+  $end->parse("$enddate $endtime") and
+    die ("Error parsing \"$enddate $endtime\": " . ($end->err()));
 
   my $event = Data::ICal::Entry::Event->new();
   $event->add_properties
     (
      summary => "Reservation for $owner"
-     , dtstart => Date::ICal->new(ical => $startdate->printf('%Y%m%dT%H%M%S')
+     , dtstart => Date::ICal->new(ical => $start->printf('%Y%m%dT%H%M%S')
 				  , offset => $offset)->ical
-     , dtend => Date::ICal->new(ical => $enddate->printf('%Y%m%dT%H%M%S')
+     , dtend => Date::ICal->new(ical => $end->printf('%Y%m%dT%H%M%S')
 				, offset => $offset)->ical
     );
   $calendar->add_entry($event);
@@ -68,6 +69,9 @@ my $csv = Text::CSV->new();
 my @rows = split("\n", $csv_raw);
 my %col2time = ();
 my $calendar = Data::ICal->new();
+my $res_owner = undef;
+my $res_startdate = undef;
+my $res_starttime = undef;
 for my $i (0 .. $#rows) {
   my $row = $rows[$i] . "\n";
 
@@ -85,9 +89,6 @@ for my $i (0 .. $#rows) {
     }
   } else {
     # This is a row of reservations
-    my $res_owner = undef;
-    my $res_starttime = undef;
-
     my $date = $fields[0];
     for my $j (1 .. $#fields) {
       my $slot_owner = $fields[$j];
@@ -101,12 +102,14 @@ for my $i (0 .. $#rows) {
 	  # I have a completed reservation to process
 	  process_reservation($calendar,
 			      $res_owner,
-			      $date,
+			      $res_startdate,
 			      $res_starttime,
+			      $date,
 			      $col2time{$j},
 			      $offset);
 
 	  $res_owner = undef;
+	  $res_startdate = undef;
 	  $res_starttime = undef;
 	}
       }
@@ -120,10 +123,29 @@ for my $i (0 .. $#rows) {
 	}
 
 	$res_owner = $slot_owner;
+	$res_startdate = $date;
 	$res_starttime = $col2time{$j};
       }
     }
   }
+}
+
+if (defined $res_owner) {
+  # If I have an active reservation here, close it at the end of the day.
+  my $enddate = new Date::Manip::Date;
+
+  $enddate->parse("$res_startdate") and
+    die ("Error parsing \"$res_startdate\": " . ($enddate->err()));
+  $enddate->next(undef, 0, [0, 0]) and
+    die ("Error going to next midnight: " . ($enddate->err()));
+
+  process_reservation($calendar,
+		      $res_owner,
+		      $res_startdate,
+		      $res_starttime,
+		      $enddate->printf('%a, %b %d'),
+		      "00:00",
+		      $offset);
 }
 
 print ($calendar->as_string());
